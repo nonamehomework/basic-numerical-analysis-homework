29d4d96287e76565c22b084a57fba628dc491e70
nonamehomework <lnsf03@gmail.com>
2020-11-14 00:04:01 +0900

    解けた

--- 02-gauss-seidel.c
+++ 02-gauss-seidel.c
@@ -4,12 +4,13 @@
 #define K 30
 #define EPS 0.0001
 int main(int argc, char const *argv[]){
-    double a[N][N+1]={{4,-1,0,0,-1,3},{-1,4,-1,0,0,3},{0,-1,4,-1,0,7},{0,0,-1,4,-1,9},{-1,0,0,-1,4,8}}, x[N]={1.0}, y[N]={1.0}, q, sum;
+    double a[N][N+1]={{4,-1,0,0,-1,3},{-1,4,-1,0,0,3},{0,-1,4,-1,0,7},{0,0,-1,4,-1,9},{-1,0,0,-1,4,8}}, x[N], y[N], q=EPS+1, sum;
     int i,j,k;
+    for(int i = 0; i < N; i++) x[i] = y[i] = 1.0;
     for(k = 1; k <= K && q >= EPS; k++){
-        for(i = 0, sum = 0; i < N; i++)
-            for(j = 0; j < N; j++, x[i] = (a[i][N]-sum) / a[i][i])if (i != j) sum += a[i][j] * x[j];
-        for(i = 0, q = 0; i < N; y[i] = x[i], i++) q += fabs(x[i]-y[i]);
+        for(i = 0; i < N; (x[i] = (a[i][N]-sum) / a[i][i]), i++)
+            for(j = 0, sum = 0; j < N; j++) if (i != j) sum += a[i][j] * x[j];
+        for(i = 0, q = 0; i < N; (y[i] = x[i]), i++) q += fabs(x[i]-y[i]);
     }
     if(k > K) printf("収束せず\n"); else for(i = 0; i < N; i++) printf("x%d = %lf\n", i, x[i]); 
 }
