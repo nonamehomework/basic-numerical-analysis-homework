68b187f40e44559a76d846ee39f0b6e4d3cf4888
nonamehomework <lnsf03@gmail.com>
2021-01-07 13:47:06 +0900

    台形法ひとまず終わり

--- 06-trapezoital.c
+++ 06-trapezoital.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <math.h>
-
+double f(double x){return 1/x;}
 int main(void){
-    printf("Hello world\n");
+    for(int n=1;n<=500;n++){
+        double a=1,b=2,s=0,h=(b-a)/n,x;
+        for(int i=0;i<n;i++) s+=2*f(a+h*i);
+        s=0.5*(s+f(a)+f(b))*h;
+        (n==1||n%100==0)&&printf("n=%3d,s=%lf,d=%lf\n",n,s,s-log(2));
+    }
 }
