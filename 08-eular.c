6b2955ea3ee146e464c146fcf1fe9f44bc3bf81e
nonamehomework <lnsf03@gmail.com>
2021-01-07 14:41:49 +0900

    少し飽きた

--- 08-eular.c
+++ 08-eular.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <math.h>
-
+double f(double x, double y){return x+y;}
 int main(void){
-    printf("Hello world\n");
+    for(int n=1;n<=10;n++){
+        double a=0,b=1,e=1,x=a,y=e,h=(b-a)/n;
+        for(;x<b;x+=h) y+=h*f(x,y);
+        (n==1||n%100==0)&& printf("n=%d, x=%lf, y=%lf\n",n,x,y);
+    }
 }
