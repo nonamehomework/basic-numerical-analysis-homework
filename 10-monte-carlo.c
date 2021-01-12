922472504e6735afd01d0b4f7dd3547027adf6ad
nonamehomework <lnsf03@gmail.com>
2021-01-12 11:45:37 +0900

    あとで

--- 10-monte-carlo.c
+++ 10-monte-carlo.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <math.h>
-
+#include <stdlib.h>
 int main(void){
-    printf("Hello world\n");
+    double m=0,x,y;
+    for(int i=1;i<=100000;i++){
+        x=(double)rand()/RAND_MAX;
+        y=(double)rand()/RAND_MAX;
+        if(i % 5000==0)
+            printf("%6d %lf\n",i,(double)m/i);
+    }
 }
