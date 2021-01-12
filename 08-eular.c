d6ea8f304ea5ec6d4706311f1497f6cc9db3e816
nonamehomework <lnsf03@gmail.com>
2021-01-12 11:31:49 +0900

    後で改善

--- 08-eular.c
+++ 08-eular.c
@@ -2,9 +2,10 @@
 #include <math.h>
 double f(double x, double y){return x+y;}
 int main(void){
-    for(int n=1;n<=10;n++){
-        double a=0,b=1,e=1,x=a,y=e,h=(b-a)/n;
+    for(int n=1;n<=10000;n++){
+        if (!(n==1||n%1000==0)) continue;
+        double a=0,b=2,e=1,x=a,y=e,h=(b-a)/n;
         for(;x<b;x+=h) y+=h*f(x,y);
-        (n==1||n%100==0)&& printf("n=%d, x=%lf, y=%lf\n",n,x,y);
+        printf("n=%d, x=%lf, y=%lf\n",n,x,y);
     }
 }
