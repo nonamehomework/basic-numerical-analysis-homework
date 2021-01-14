c48431065027ba72b8ad426a17238778d899b02e
nonamehomework <lnsf03@gmail.com>
2021-01-14 14:06:20 +0900

    表示ミス

--- 06-trapezoital.c
+++ 06-trapezoital.c
@@ -7,6 +7,6 @@ int main(void){
         double a=1,b=2,s=0,h=(b-a)/n,x;
         for(int i=0;i<n;i++) s+=2*f(a+h*i);
         s=0.5*(s+f(a)+f(b))*h;
-        printf("n=%3d,s=%lf,d=%lf\n",n,s,s-log(2));
+        printf("n=%4d,s=%lf,d=%lf\n",n,s,s-log(2));
     }
 }
